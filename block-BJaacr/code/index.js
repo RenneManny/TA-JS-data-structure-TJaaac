06d79d9f25570912226119c97cc2ab9d94c685cc
Akhil Kumar <akhilkum249@gmail.com>
2023-11-27 15:39:47 +0530

    Assignment-3

--- block-BJaacr/code/index.js
+++ block-BJaacr/code/index.js
@@ -19,13 +19,14 @@ Hint: (Structure of got variable)
 */
 
 // 1. Log the value of key "houses"
-
+console.log(got.houses);
 // 2. Log the length of the "houses" key
-
+console.log(got.houses.length);
 // 3. Log the name of all houses in got one by one
-
+console.log(got.houses[0].name);
+console.log(got.houses[1].name);
 // 4. Log the name of all the people in house Starks (index 0) one by one
-
+console.log(got.houses[0].people[people.length-1].name)
 // 5. Log the name of all the people in house Lannisters (index 1) one by one
 
 // 6. Log the description of all the people in house Lannisters (index 1) one by one
